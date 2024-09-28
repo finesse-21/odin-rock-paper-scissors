a96893fc0222612065ae02c5626a770a75224034
finesse-21 <21pepperforreal@gmail.com>
2024-09-28 16:07:04 +0300

    Make human choice function

--- script.js
+++ script.js
@@ -2,8 +2,16 @@
 
 function getComputerChoice() {
     let rand = Math.floor(Math.random() * 3);
-    if (rand == 0) { return "Rock" };
-    if (rand == 1) { return "Paper"};
-    return "Scissors";
+    if (rand == 0) { return "rock" };
+    if (rand == 1) { return "paper"};
+    return "scissors";
 }
 console.log(getComputerChoice());
+
+function getHumanChoice() {
+    let choice = prompt("Choose your weapon!").trim().toLowerCase();
+    if (choice === "rock" || choice === "paper" || choice === "scissors") {
+        return choice; 
+    }
+    return alert("You entered wrong weapon :(");
+}
