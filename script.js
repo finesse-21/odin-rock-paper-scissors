e82219a538155208cb3cd82863924a7f52c62dfa
finesse-21 <21pepperforreal@gmail.com>
2024-09-28 16:30:45 +0300

    Create playRound function

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-
-
 function getComputerChoice() {
     let rand = Math.floor(Math.random() * 3);
     if (rand == 0) { return "rock" };
     if (rand == 1) { return "paper"};
     return "scissors";
 }
-console.log(getComputerChoice());
 
 function getHumanChoice() {
     let choice = prompt("Choose your weapon!").trim().toLowerCase();
@@ -15,3 +12,26 @@ function getHumanChoice() {
     }
     return alert("You entered wrong weapon :(");
 }
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        return console.log(`It is a draw! ${humanChoice} to ${computerChoice}`);
+    }
+    if (humanChoice === "paper" && computerChoice === "scissors" 
+        || humanChoice === "scissors" && computerChoice === "rock"
+        || humanChoice === "rock" && computerChoice === "paper"
+    ) {
+        computerScore += 1;
+        return console.log(`You Lose! ${computerChoice} beats ${humanChoice}`);
+    }
+    humanScore += 1;
+    return console.log(`You Win! ${humanChoice} beats ${computerChoice}`);
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
