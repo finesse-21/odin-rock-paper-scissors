f9b966a78b38a3534130f5bf1b78228a609c8584
finesse-21 <21pepperforreal@gmail.com>
2024-09-28 15:49:15 +0300

    Make computer choice function

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Testing")
+
+
+function getComputerChoice() {
+    let rand = Math.floor(Math.random() * 3);
+    if (rand == 0) { return "Rock" };
+    if (rand == 1) { return "Paper"};
+    return "Scissors";
+}
+console.log(getComputerChoice());
