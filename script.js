bd9f96a6e415ee7cd2ba11056512bd808df42589
finesse-21 <21pepperforreal@gmail.com>
2024-09-30 14:49:36 +0300

    Add UI

--- script.js
+++ script.js
@@ -4,44 +4,74 @@ function getComputerChoice() {
     if (rand == 1) { return "paper"};
     return "scissors";
 }
-
-function getHumanChoice() {
-    let choice = prompt("Choose your weapon!").trim().toLowerCase();
-    if (choice === "rock" || choice === "paper" || choice === "scissors") {
-        return choice; 
-    }
-    return alert("You entered wrong weapon :(");
+let score = [0, 0] 
+function winCheck(score) {
+    if (score[0] == 5) {
+        alert("You won!");
+     }
+     if (score[1] == 5) {
+        alert("You Lose!");
+     }
 }
+const rockButton = document.querySelector(".rock");
+const paperButton = document.querySelector(".paper");
+const scissorsButton = document.querySelector(".scissors");
+rockButton.addEventListener("click", () => {
+    playRound("rock", getComputerChoice(), score);
+  });
+paperButton.addEventListener("click", () => {
+    playRound("paper", getComputerChoice(), score);
+});
+scissorsButton.addEventListener("click", () => {
+    playRound("scissors", getComputerChoice(), score);
+});
 
-function playRound(humanChoice, computerChoice, humanScore, computerScore) {
+const scoreContainer = document.querySelector(".score");
+scoreContainer.textContent = `Current score is ${score[0]} to ${score[1]}`
+const resultContainer = document.querySelector(".result");
+function playRound(humanChoice, computerChoice, score) {
     if (humanChoice === computerChoice) {
-        return console.log(`It is a draw! ${humanChoice} to ${computerChoice}`);
+        return resultContainer.textContent = `It is a draw! ${score[0]} to ${score[1]}`;
     }
     if (humanChoice === "paper" && computerChoice === "scissors" 
         || humanChoice === "scissors" && computerChoice === "rock"
         || humanChoice === "rock" && computerChoice === "paper"
     ) {
-        console.log(`You Lose! ${computerChoice} beats ${humanChoice}. Score is ${humanScore} to ${computerScore + 1}`);
-        return 0;        
+        resultContainer.textContent = `You Lose! ${computerChoice} beats ${humanChoice}. Score is ${score[0]} to ${score[1] + 1}`;
+        scoreContainer.textContent = `Current score is ${score[0]} to ${score[1] + 1}`
+        winCheck([score[0], score[1] + 1])
+        return score[1] += 1;        
     }
-    console.log(`You Win! ${humanChoice} beats ${computerChoice}. Score is ${humanScore + 1} to ${computerScore}`);
-    return 1;
+    resultContainer.textContent = `You Win! ${humanChoice} beats ${computerChoice}. Score is ${score[0] + 1} to ${score[1]}`;
+    scoreContainer.textContent = `Current score is ${score[0] + 1} to ${score[1]}`
+    winCheck([score[0] + 1, score[1]])
+    return score[0] += 1;
 }
 
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
-    let round = 0;
-    for (let i = 0; i < 5; i++) {
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        round = playRound(humanSelection, computerSelection, humanScore, computerScore);
-        if (round === 0 ) {computerScore += 1;}
-        else if (round === 1) {humanScore += 1;}        
-    }
-    if (humanScore > computerScore) {console.log("You won the game!"); }
-    else if (humanScore < computerScore) {console.log("You lose the game!");}
-    else {console.log("It's a draw!")}
-    return console.log(`Score is ${humanScore} to ${computerScore}`)
-}
-playGame();
+
+
+
+
+
+// const playBtn = document.querySelector(".play");
+// playBtn.addEventListener("click", () => {
+//   playRound(getHumanChoice(), getComputerChoice(), 0, 0);
+// });
+
+// function playGame() {
+//     let humanScore = 0;
+//     let computerScore = 0;
+//     let round = 0;
+//     for (let i = 0; i < 5; i++) {
+//         const humanSelection = getHumanChoice();
+//         const computerSelection = getComputerChoice();
+//         round = playRound(humanSelection, computerSelection, humanScore, computerScore);
+//         if (round === 0 ) {computerScore += 1;}
+//         else if (round === 1) {humanScore += 1;}        
+//     }
+//     if (humanScore > computerScore) {console.log("You won the game!"); }
+//     else if (humanScore < computerScore) {console.log("You lose the game!");}
+//     else {console.log("It's a draw!")}
+//     return console.log(`Score is ${humanScore} to ${computerScore}`)
+// }
+// playGame();
